adc301cffe74106db8a887b4f682fada5b1321f9
Saarthak <saarthakgupta08@gmail.com>
2019-11-04 15:10:08 +0530

    Added Project Files

---
+++ src/main/java/com/sg/fsp/FormSubmissionPortalApplication.java
@@ -0,0 +1,13 @@
+package com.sg.fsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSubmissionPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormSubmissionPortalApplication.class, args);
+	}
+
+}
